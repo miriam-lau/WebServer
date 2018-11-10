a47650afd4699b3775680b75c4da81f0f13dd796
James Lee <jameslee.jl@gmail.com>
2018-11-10 11:58:27 -0800

    Ran vue init webpack frontend. Used Standard for ESLint. Changed HelloWorld.vue to Home.vue

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 
-
 USERNAME_COOKIE_NAME = "username"
 DEFAULT_USERNAME = "guest"
 
@@ -13,10 +12,10 @@ def render_homepage():
     username = DEFAULT_USERNAME
     if USERNAME_COOKIE_NAME in request.cookies:
         username = request.cookies.get(USERNAME_COOKIE_NAME)
-    return render_template('homepage.html', **locals())
+    return render_template("homepage.html", **locals())
 
 
-@app.route('/setcookie', methods=['POST'])
+@app.route("/setcookie", methods=["POST"])
 def set_username_cookie():
     username = request.form[USERNAME_COOKIE_NAME]
     resp = make_response(redirect(url_for("render_homepage")))
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home page'
+    }
+  }
+}
+</script>
